c3a4ce2ec1837369921e25fc8c0c11786a15fda0
mr13 <mr13@DESKTOP-PN0DMCN>
2024-07-27 10:59:54 +0400

    MSSQL Datatypes VS C# Datatypes

---
+++ ADO 08. C# VS MSSQL datatypes/Program.cs
@@ -0,0 +1,48 @@
+﻿// MSSQL Datatypes VS C# Datatypes
+// https://learn.microsoft.com/en-us/dotnet/framework/data/adonet/sql-server-data-type-mappings
+/*
+    SQL                     C#
+ 
+    tinyint                 byte (Byte)
+    smallint                short (Int16)
+    int                     int (Int32)
+    bigint                  long (Int64)
+
+
+    bit                     bool (Boolean)
+
+
+    decimal                 decimal (Decimal)
+    numeric                 decimal (Decimal)
+ 
+
+    smallmoney              decimal (Decimal)
+    money                   decimal (Decimal)
+
+
+    real                    float (Single)
+    float                   double (Double)
+ 
+    
+    date                    Datetime
+    time                    TimeSpan
+    datetime2               Datetime
+    datetimeoffset          DateTimeOffset
+    datetime                Datetime
+    smalldatetime           Datetime
+    
+
+
+    char                    string (String) - Char[]
+    varchar                 string (String) - Char[]
+    text                    string (String) - Char[]
+    nchar                   string (String) - Char[]
+    nvarchar                string (String) - Char[]
+    ntext                   string (String) - Char[]
+
+
+
+    binary                  Byte[]
+    varbinary               Byte[]
+    image                   Byte[]
+ */
